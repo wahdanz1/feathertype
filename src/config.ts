572491a856fa582188eb2a9781b2477e11e635e8
Daniel Wahlgren <dwahlgren93@gmail.com>
2026-05-22 15:00:01 +0200

    release: v1.1.0

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const APP_NAME = 'FeatherType';
-export const APP_VERSION = '1.0.0';
+export const APP_VERSION = '1.1.0';
 
 export const LINKS = {
   website: 'https://feathertype.vercel.app',
